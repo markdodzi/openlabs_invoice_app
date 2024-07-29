27cd3ee0cac2de46984dd2586650bea7b2f3a07f
Mark <markdodzi@gmail.com>
2024-07-29 17:25:38 +0900

    mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
         { <AcmeLogo /> }
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-300 px-6 py-10 md:w-2/5 md:px-20">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-blue-300  px-6 py-10 md:w-2/5 md:px-20">
         <div
   className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
 />
@@ -40,6 +40,13 @@ export default function Page() {
         height={760}
         className="hidden md:block"
         alt="Screenshots of the dashboard project showing desktop version"
+      />
+      <Image
+        src="/11.jpg"
+        width={560}
+        height={620}
+        className="block md:hidden"
+        alt="Screenshot of the dashboard project showing mobile version"
       />
         </div>
       </div>
